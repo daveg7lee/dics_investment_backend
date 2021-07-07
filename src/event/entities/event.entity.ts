5f18a4c5f747c7a0f30ebfa41dbf35031da4d159
Dave <daveg7lee@gmail.com>
2021-07-07 18:33:31 +0900

    [FIX] Id type

--- src/event/entities/event.entity.ts
+++ src/event/entities/event.entity.ts
@@ -1,10 +1,12 @@
 import { Field, InputType, ObjectType } from '@nestjs/graphql';
-import { CoreEntity } from 'src/common/entities/core.entity';
 import { User } from 'src/user/entities/user.entity';
 
 @InputType('EventInputType', { isAbstract: true })
 @ObjectType()
-export class Event extends CoreEntity {
+export class Event {
+  @Field((type) => Number)
+  id: Number;
+
   @Field((type) => String)
   title: string;
 
